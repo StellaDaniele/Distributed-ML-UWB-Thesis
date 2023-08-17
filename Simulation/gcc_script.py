cdde93cf3745abd7195028f7bd36a0319da12d7c
StellaDaniele <stella.dani1@gmail.com>
2023-08-17 17:29:11 +0200

    Fixed bugs, now compilation in batches

--- Simulation/gcc_script.py
+++ Simulation/gcc_script.py
@@ -1,10 +1,14 @@
 import os
 
-# Paths and compilation
-file_compilation_strings = "compilation_strings.bat"
+# Paths and script settings
 directory_executables = "./Compiled"
-source_code_files = "../Autonomous_Edge_Pipeline/*.c"
-gcc_warning_suppression_options = "2>nul"
+directory_compilation_strings = "./Compilation_Strings"
+source_code_files = "../../Autonomous_Edge_Pipeline/*.c"
+gcc_warning_suppression_options = "" # "2>nul"
+file_name_compilation = "compilation_strings"
+file_name_compilation_prefix = directory_compilation_strings + "/" + file_name_compilation
+extension_file_compilation = ".bat"
+batch_dimension_compilation_files = 1000
 
 # Static values
 K = 2   # Number of clusters k-means
@@ -18,8 +22,11 @@ def generate_compilation_strings():
     FILTERS = ['CONF','FIFO','RANDOM']
 
     output_file_names = []
-
-    output_file = open(file_compilation_strings, "w")
+    compilation_files_counter = 0
+    current_strings_on_file = 0
+    total_compilation_strings_generated = 0
+    output_file = open(file_name_compilation_prefix+str(compilation_files_counter)+extension_file_compilation, "w")
+    compilation_files_counter += 1
 
     for N_NODES in range(1,6):
         N_TRAIN_USED = N_TRAIN // N_NODES
@@ -29,7 +36,7 @@ def generate_compilation_strings():
                 while MEMORY_SIZE <= N_TRAIN_USED:
                     min_initial_thr = min(MEMORY_SIZE//2,50)
                     max_initial_thr = MEMORY_SIZE // 2
-                    NODE_OFFSET = MEMORY_SIZE/N_NODES*(NODE_ID-1)
+                    NODE_OFFSET = MEMORY_SIZE//N_NODES*(NODE_ID-1)
                     for CONFIDENCE in range(2): # False / True
                         for CONFIDENCE_THR in (CONF_THR_LIST if CONFIDENCE else [0]):
                             # The condition above should limit all the useless cases
@@ -47,16 +54,17 @@ def generate_compilation_strings():
                                                 current_value *= 2
                                         for UPDATE_THR in update_thr_values:
                                             # TODO: Decide how to increase: is 5*2^i ok?
-                                            # TODO: Decide order, add everything to string, remember SIMULATION macro
-                                            name_curr = f"{N_NODES}_{NODE_ID}_{K_NEIGHBOR}_{MEMORY_SIZE}_{CONFIDENCE}_{CONFIDENCE_THR}_{FILTER}_{ONE_SHOT}_{INITIAL_THR}_{UPDATE_THR}_{K}_{ITERATION}_{N_TRAIN}_{N_TRAIN_USED}_{N_TEST}_{N_TEST_USED}"
+                                            name_curr = f"{N_NODES}_{K_NEIGHBOR}_{MEMORY_SIZE}_{NODE_OFFSET}_{CONFIDENCE}_{CONFIDENCE_THR}_{FILTER}_{ONE_SHOT}_{INITIAL_THR}_{UPDATE_THR}_{K}_{ITERATION}_{N_TRAIN}_{N_TRAIN_USED}_{N_TEST}_{N_TEST_USED}_{NODE_ID}"
+                                            # NODE_ID is last so that we can use sorting to find all the nodes given the settings
                                             compilation_string = f"gcc -D SIMULATION "\
                                             f"-D N_NODES={N_NODES} "\
                                             f"-D NODE_ID={NODE_ID} "\
                                             f"-D K_NEIGHBOR={K_NEIGHBOR} "\
                                             f"-D MEMORY_SIZE={MEMORY_SIZE} "\
+                                            f"-D NODE_OFFSET={NODE_OFFSET} "\
                                             f"-D CONFIDENCE={CONFIDENCE} "\
                                             f"-D CONFIDENCE_THR={CONFIDENCE_THR} "\
-                                            f"-D FILTER={FILTER} "\
+                                            f"-D {FILTER} "\
                                             f"-D ONE_SHOT={ONE_SHOT} "\
                                             f"-D INITIAL_THR={INITIAL_THR} "\
                                             f"-D UPDATE_THR={UPDATE_THR} "\
@@ -66,24 +74,32 @@ def generate_compilation_strings():
                                             f"-D N_TRAIN_USED={N_TRAIN_USED} "\
                                             f"-D N_TEST={N_TEST} "\
                                             f"-D N_TEST_USED={N_TEST_USED} "\
-                                            f"-D SETTINGS=\"{name_curr}\" "\
+                                            f"-D SETTINGS=\\\"{name_curr}\\\" "\
                                             f"{gcc_warning_suppression_options} "\
                                             f"{source_code_files} "\
                                             f"-o {directory_executables}/{name_curr}"
                                             output_file.write(compilation_string + "\n")
+                                            current_strings_on_file += 1
+                                            total_compilation_strings_generated += 1
+                                            if(current_strings_on_file % batch_dimension_compilation_files == 0):
+                                                output_file.close()
+                                                output_file = open(file_name_compilation_prefix+str(compilation_files_counter)+extension_file_compilation, "w")
+                                                compilation_files_counter += 1
+                                                current_strings_on_file = 0
                                             output_file_names.append(name_curr)
                     MEMORY_SIZE *= 2 # TODO: Decide how to increase: is 10*2^i ok?
-    return output_file_names
+    return output_file_names, compilation_files_counter, total_compilation_strings_generated
 
 if __name__ == "__main__":
     if not os.path.exists(directory_executables):
         os.makedirs(directory_executables)
 
     # Gcc compilation string generation
-    output_file_names = generate_compilation_strings()
-    for file_name in output_file_names:
-        print(file_name)
-
+    output_file_names, compilation_files_counter, total_compilation_strings_generated = generate_compilation_strings()
+    # for file_name in output_file_names:
+    #     print(file_name)
+    print("# Files: "+str(compilation_files_counter))
+    print("# Compilation strings: "+str(total_compilation_strings_generated))
     # # Gcc compilation execution
     # os.system(file_compilation_strings)
 

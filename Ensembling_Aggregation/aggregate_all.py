2096d38d965dfbeffb5eb1f6ad504436549ef0ad
StellaDaniele <stella.dani1@gmail.com>
2023-08-22 11:21:12 +0200

    Added stats and aggregator call

--- Ensembling_Aggregation/aggregate_all.py
+++ Ensembling_Aggregation/aggregate_all.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from ensemble_aggregator import aggregator
+
 folder_path = "../Others/prova/"
 
 def extract_data(folder_name):
@@ -19,27 +21,39 @@ def extract_data(folder_name):
     n_nodes = len(nodes_data)
 
     centroids = []
-    correctly_classified = []
-    accuracies = []
+    correctly_classified_before = []
+    accuracies_before = []
 
     for node in nodes_data:
         first_iteration = node["pipeline_iterations"][0]
         centroids.append(first_iteration["final_centroids"])
-        correctly_classified.append(first_iteration["correctly_classified_samples"])
-        accuracies.append(first_iteration["accuracy"])
+        correctly_classified_before.append(first_iteration["correctly_classified_samples"])
+        accuracies_before.append(first_iteration["accuracy"])
 
     n_tests = len(nodes_data[0]["pipeline_iterations"][0]["test_data"])
     n_neighbors = len(nodes_data[0]["pipeline_iterations"][0]["test_data"][0]["neighbors"])
     neighbors = [[None] * n_neighbors for _ in range(len(nodes_data))]
     scores = [[None] * n_neighbors for _ in range(len(nodes_data))]
+    correctly_classified = 0
 
     for test in range(n_tests):
         test_coordinates = nodes_data[0]["pipeline_iterations"][0]["test_data"][test]["test_coordinates"]
-        print(test_coordinates)
+        #print(test_coordinates)
         for node in range(n_nodes):
             for neighbor in range(n_neighbors):
                 neighbors[node][neighbor] = nodes_data[0]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["coordinates"]
                 scores[node][neighbor] = nodes_data[0]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["score"]
+        correctly_classified += int(aggregator(n_nodes, centroids, n_neighbors, neighbors, test, test_coordinates, scores))
+
+    accuracy = correctly_classified/n_tests
+
+    print("Before aggregation:")
+    print("\tCorrectly classified = "+str(correctly_classified_before))
+    print("\tAccuracy="+str(accuracies_before))
+    print("After aggregation:")
+    print("\tCorrectly classified = "+str(correctly_classified))
+    print("\tAccuracy="+str(accuracy))
+
 
     print()
 
@@ -50,4 +64,3 @@ if __name__ == "__main__":
         subfolder_path = os.path.join(folder_path, subfolder_name)
         if os.path.isdir(subfolder_path):
             extract_data(subfolder_name)
-

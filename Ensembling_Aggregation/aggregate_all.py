b97b39e4e144798fe23d08e44601af7a007c60b6
StellaDaniele <stella.dani1@gmail.com>
2023-09-03 04:10:45 +0200

    Now saving everything on the lists for all algorithms

--- Ensembling_Aggregation/aggregate_all.py
+++ Ensembling_Aggregation/aggregate_all.py
@@ -3,22 +3,24 @@ import os
 import csv
 
 from ensemble_aggregator import aggregator_scores
+from ensemble_aggregator import aggregator_coordinates
 
 folder_path = "../Others/prova/"
 
 def extract_data(folder_name, writer):
-    path = folder_path+folder_name
+    path = folder_path + folder_name
     json_file_names = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('.json')]
 
     sorted_json_file_names = sorted(json_file_names)
     nodes_data = []
 
     for json_file_name in sorted_json_file_names:
-        json_file = open(path+"/"+json_file_name, "r")
-        nodes_data.append(json.load(json_file))
-        json_file.close()
+        with open(path + "/" + json_file_name, "r") as json_file:
+            nodes_data.append(json.load(json_file))
 
     n_nodes = len(nodes_data)
+    n_tests = len(nodes_data[0]["pipeline_iterations"][0]["test_data"])
+    n_neighbors = len(nodes_data[0]["pipeline_iterations"][0]["test_data"][0]["neighbors"])
 
     centroids = []
     correctly_classified_before = []
@@ -29,25 +31,32 @@ def extract_data(folder_name, writer):
         centroids.append(first_iteration["final_centroids"])
         correctly_classified_before.append(first_iteration["correctly_classified_samples"])
         accuracies_before.append(first_iteration["accuracy"])
-    n_tests = len(nodes_data[0]["pipeline_iterations"][0]["test_data"])
-    n_neighbors = len(nodes_data[0]["pipeline_iterations"][0]["test_data"][0]["neighbors"])
-    neighbors = [[None] * n_neighbors for _ in range(len(nodes_data))]
-    scores = [[None] * n_neighbors for _ in range(len(nodes_data))]
-    neighbors_labels = [[None] * n_neighbors for _ in range(len(nodes_data))]
-    correctly_classified = 0
+
+    neighbors = [[[None] * n_neighbors for _ in range(n_nodes)] for _ in range(n_tests)]
+    scores = [[[None] * n_neighbors for _ in range(n_nodes)] for _ in range(n_tests)]
+    neighbors_labels = [[[None] * n_neighbors for _ in range(n_nodes)] for _ in range(n_tests)]
+
+    correctly_classified_scores = 0
+    correctly_classified_coordinates = 0
+    correctly_classified_coordinates_normalization = 0
     test_coordinates = []
 
     for test in range(n_tests):
         test_coordinates.append(nodes_data[0]["pipeline_iterations"][0]["test_data"][test]["test_coordinates"])
         for node in range(n_nodes):
             for neighbor in range(n_neighbors):
-                neighbors[node][neighbor] = nodes_data[node]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["coordinates"]
-                scores[node][neighbor] = nodes_data[node]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["score"]
-                neighbors_labels[node][neighbor] = nodes_data[node]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["label"]
-        correctly_classified += int(aggregator_scores(n_nodes, n_neighbors, neighbors, test_coordinates, test,scores, neighbors_labels))
-    accuracy = correctly_classified/n_tests
+                neighbors[test][node][neighbor] = nodes_data[node]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["coordinates"]
+                scores[test][node][neighbor] = nodes_data[node]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["score"]
+                neighbors_labels[test][node][neighbor] = nodes_data[node]["pipeline_iterations"][0]["test_data"][test]["neighbors"][neighbor]["label"]
 
-    writer.writerow([folder_name]+folder_name.split('_')[1:]+[accuracies_before,accuracy])
+        correctly_classified_scores += int(aggregator_scores(n_nodes, n_neighbors, neighbors[test], test_coordinates, test, scores[test], neighbors_labels[test]))
+    accuracy_score = correctly_classified_scores / n_tests
+
+    correctly_classified_coordinates = aggregator_coordinates(n_nodes, centroids, n_neighbors, neighbors, test_coordinates, scores, neighbors_labels)
+    accuracy_coordinates = correctly_classified_coordinates / n_tests
+    # Print on CSV the folder name, the settings (1 per column), and the accuracies
+    writer.writerow([folder_name] + folder_name.split('_')[1:] + [accuracies_before, accuracy_score, accuracy_coordinates])
+
 
 
 if __name__ == "__main__":
